fcd2bc3c9576bbd2be04893aaceb5ee9bf019226
Jason Ong <jasononguk@gmail.com>
2025-10-10 16:27:37 +0100

    Initial Commit

---
+++ JamAI/JamAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JamAIApp.swift
+//  JamAI
+//
+//  Created by Jason Ong on 10/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct JamAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JamAITests/JamAITests.swift
@@ -0,0 +1,17 @@
+//
+//  JamAITests.swift
+//  JamAITests
+//
+//  Created by Jason Ong on 10/10/2025.
+//
+
+import Testing
+@testable import JamAI
+
+struct JamAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
